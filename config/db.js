8e6a107ddcf825d7af5b69640ead26c8de2820e2
Mantu98 <mantu07941@gmail.com>
2024-04-19 20:27:32 +0530

    mongodb updated

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import env from "dotenv";
 
+env.config();
 
 const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`CONNECT TO Mongodb ${connect.connection.host}`);
-    } catch (error) {
-        console.log(`error in mogoDB ${error}`);
-    }
-}
-
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`CONNECT TO Mongodb ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`error in mogoDB ${error}`);
+  }
+};
 
 export default connectDB;

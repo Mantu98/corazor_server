364c84cbf99c41ec2b670207dccded5765403efe
Mantu98 <mantu07941@gmail.com>
2024-04-30 21:37:39 +0530

    server update

--- models/userModel.js
+++ models/userModel.js
@@ -1,31 +1,67 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: false
+//     },
+//     phone:{
+//         type: Number,
+//         required: true,
+//         unique: false
+//     },
+
+//     message:[{
+//         message: {type: String, required: true},
+//         preferedServices:[{type: String, required: true}],
+//         createdAt: {type: Date, default: Date.now()}
+//     }],
+
+// },{timestamps:true})
+
+// const userModel = mongoose.model('user', userSchema);
+
+// export default userModel;
+
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
 
     email: {
-        type: String,
-        required: true,
-        unique: false
+      type: String,
+      required: true,
+      unique: false,
     },
-    phone:{
-        type: Number,
-        required: true,
-        unique: false
+    phone: {
+      type: Number,
+      required: true,
+      unique: false,
     },
 
-    message:[{
-        message: {type: String, required: true},
-        preferedServices:[{type: String, required: true}],
-        createdAt: {type: Date, default: Date.now()}
-    }],
-    
-},{timestamps:true})
+    message: [
+      {
+        message: { type: String, required: true },
+        preferedServices: [{ type: String, required: true }],
+        createdAt: { type: Date, default: Date.now() },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-const userModel = mongoose.model('user', userSchema);
+const userModel = mongoose.model("user", userSchema);
 
 export default userModel;
